c6a12c938188375275ac5f1f8c134dc4e21f5100
Jonas Diel <jonasdiel@gmail.com>
2017-08-04 13:29:37 -0300

    Criada API #1
    Métodos de login, logout, cadastrar novo usuario, atualizar dados do usuario e obter dados do usuário

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Anuncio;
+use App\Http\Controllers\Auth\AuthController;
+use App\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class ApiController extends Controller
+{
+    /**
+    * Verifica as credenciais com email e password
+    * @param Request $request
+    * @return string FALSE quando credenciais nao correspondem ou o HASH quando autenticação procede
+    */
+   public function login(Request $request)
+   {
+       //Verificar credenciais
+       $credentials = $request->only('email', 'password');
+       if( Auth::attempt($credentials, false) )
+       {
+           //Gerar e atualizar token
+           $token = str_replace('/','',Hash::make('plain-text'));
+
+           $user = User::where('email', $request->email)->first();
+           $user->api_token = $token;
+           $user->save();
+
+           return Response::json($token);
+       }
+
+       return Response::json(false);
+   }
+
+   /**
+    * logout da API
+    * Apaga o token do usuário logado
+    * @param Request $request
+    * @return string
+    */
+   public function logout(Request $request)
+   {
+       $user = User::where('api_token', $request->api_token)->first();
+       if( !empty($user) )
+       {
+           $user->api_token = NULL;
+           $user->save();
+       }
+
+       return Response::json(true);
+   }
+
+   public function obterDadosUsuario(Request $request)
+   {
+       $user = User::where('api_token', $request->input('api_token'))->first();
+       return Response::json($user);
+   }
+
+   public function criarUsuario(Request $request)
+   {
+       $data = $request->all();
+       $data['password'] = Hash::make($request['password']);
+       try
+       {
+           $user = User::create($data);
+
+       }
+       catch (Exception $e)
+       {
+           return Response::json(['error' => $e->getMessage()]);
+       }
+
+       return $this->login($request);
+   }
+
+   public function atualizarUsuario(Request $request)
+   {
+       $user = User::where('api_token', $request->input('api_token'))->first();
+       $user->fill($request->all());
+       $user->save();
+
+       return Response::json(true);
+   }
+}
--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,49 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+/**
+ * Autenticacao (/api/auth/)
+ */
+Route::group(['prefix' => 'auth'], function()
+{
+    /**
+     * Login
+     * /api/auth/login
+     */
+    Route::post('login', 'ApiController@login');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::group(['middleware' => 'api'], function()
+    {
+        /**
+         * Logout
+         * /api/auth/logout
+         */
+        Route::post('logout', 'ApiController@logout');
+    });
+});
+
+/**
+ * Usuário (/api/usuario/)
+ */
+Route::group(['prefix' => 'usuario'], function()
+{
+    /**
+     * Cadastra novo usuário
+     * // /api/user/register
+     */
+    Route::post('cadastrar', 'ApiController@criarUsuario');
+
+    Route::group(['middleware' => 'api'], function()
+    {
+        /**
+         * Atualiza perfil do usuário
+         * /api/user/register
+         */
+        Route::post('atualizar', 'ApiController@atualizarUsuario');
+        /**
+         * Obter dados do usuário
+         * /api/user/obterDados
+         */
+        Route::get('obterDados', 'ApiController@obterDadosUsuario');
+    });
 });
